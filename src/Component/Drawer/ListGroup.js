7de02e23a62997d6111f1b6e7bdd75452bc93f2b
maxam2017 <whk0313@gmail.com>
2019-11-17 04:03:57 +0800

    refactor(components): be simpler

--- src/Component/Drawer/ListGroup.js
+++ src/Component/Drawer/ListGroup.js
@@ -1,60 +1,37 @@
 import React from 'react';
 import classNames from 'classnames';
-import {connect} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {fetchExam} from '../../Redux/Action/exam';
 import { Label, List } from './style';
 
-class ListGroup extends React.Component {
-  constructor (props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-  }
+const ListGroup = ({ order, label, list }) => {
+  const { toggles, chooseType, selectID, chaos } = useSelector(state => state.main);
+  const dispatch = useDispatch();
+  const toggle = type => () => {
+    dispatch({type: 'TOGGLE', category: type});
+  };
+  const fetch = cos => () => {
+    dispatch(fetchExam(cos));
+  };
 
-  toggle (type) {
-    this.props.toggle(type);
-  }
+  return (
+    <>
+      <Label onClick={toggle(order)}>
+        <i className={classNames('fas', (toggles[order] && (chaos || chooseType === order)) ?'fa-caret-down':'fa-caret-right')}/>
+      &nbsp;{label}
+      </Label>
+      {toggles[order] && (chaos || chooseType === order) && list.map(
+        (cos, index) => (
+          <List
+            key={index}
+            active={selectID === cos.id}
+            onClick={fetch(cos)}>
+            {cos.zh}
+          </List>
+        )
+      )}
+    </>
+  );
+};
 
-  render () {
-    const {
-      toggles,
-      chaos,
-      type,
-      order,
-      label,
-      list,
-      selectID,
-      fetchExam } = this.props;
-    return (
-      <React.Fragment>
-        <Label onClick={() => this.toggle(order)}>
-          <i className={classNames('fas',(toggles[order] && (chaos || type === order)) ?'fa-caret-down':'fa-caret-right')}/>
-        &nbsp;{label}
-        </Label>
-        {toggles[order] && (chaos || type === order) && list.map(
-          (cos, index) => (
-            <List
-              key={index}
-              active={selectID === cos.id}
-              onClick={() => fetchExam(cos)}>
-              {cos.zh}
-            </List>
-          )
-        )}
-      </React.Fragment>
-    );
-  }
-}
-
-const mapState2Prop = state => ({
-  toggles: state.main.toggles,
-  type: state.main.chooseType,
-  selectID: state.main.chooseCourse,
-  chaos: state.main.chaos
-});
-
-const mapDispatch2Prop = dispatch => ({
-  fetchExam: (id) => dispatch(fetchExam(id)),
-  toggle: (type) => dispatch({type: 'TOGGLE', category: type})
-});
-
-export default connect(mapState2Prop, mapDispatch2Prop)(ListGroup);
+export default ListGroup;
