59965e83b0da930e3e9b3514ad546131f2306a93
maxam2017 <whk0313@gmail.com>
2019-11-17 03:14:58 +0800

    feat(uploader): use native drag & drop api

--- src/Component/Upload/index.js
+++ src/Component/Upload/index.js
@@ -1,37 +1,26 @@
-import React from 'react'
-import Input from '../Input'
-import classNames from 'classnames'
-import Dropzone from 'react-dropzone'
-import injectSheet from 'react-jss'
-import style from './style'
-import axios from 'axios'
-import { closeModal } from '../Modal'
-import { connect } from 'react-redux'
-import { COURSE_TYPES } from '../../Page/Main'
-import { toast } from '../Toast'
+import React from 'react';
+import Input from '../Input';
+import {
+  Header,
+  Title,
+  Leave,
+  Action,
+  InputContainer,
+} from './style';
+import axios from 'axios';
+import { closeModal } from '../Modal';
+import { connect } from 'react-redux';
+import { COURSE_TYPES } from '../../Page/Main';
+import { toast } from '../Toast';
+import Loading from '../Loading';
+import Uploader from './Uploader';
 
-const baseURL = 'https://csunion.nctu.me/_api/oldexam'
-
-const Loading = () => (
-  <svg x='0px' y='0px' width='40px' height='65px' viewBox='0 0 50 50'>
-    <path fill='white' d='M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z'>
-      <animateTransform
-        attributeType='xml'
-        attributeName='transform'
-        type='rotate'
-        from='0 25 25'
-        to='360 25 25'
-        dur='0.6s'
-        repeatCount='indefinite' />
-    </path>
-  </svg>
-)
+const baseURL = 'https://csunion.nctu.me/_api/oldexam';
 
 class uploadModal extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
-      seq: 0,
       files: [],
       instructor: '',
       course: '',
@@ -39,138 +28,89 @@ class uploadModal extends React.Component {
       type: '',
       category: '',
       loading: false,
-    }
-    this.handleDrop = this.handleDrop.bind(this)
-    this.handleDel = this.handleDel.bind(this)
-    this.handleUpload = this.handleUpload.bind(this)
+    };
+    this.handleDrop = this.handleDrop.bind(this);
+    this.handleDel = this.handleDel.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
   handleDrop (files) {
     if (files.length > 0) {
-      let updatedFiles = this.state.files
-      let {seq} = this.state
-      files.forEach(file => {
-        updatedFiles.push({
-          id: seq,
-          name: file.name,
-          preview: file.preview,
-          size: Number.parseFloat(file.size / 1000000).toFixed(2),
-          type: file.type.split('/')
-        })
-        seq = seq + 1
-      })
-      this.setState({seq: seq, files: updatedFiles})
+      this.setState({files: files});
     }
   }
-  handleDel (e, id) {
-    let updatedFiles = this.state.files
-    updatedFiles = updatedFiles.filter(file => file.id !== id)
-    this.setState({files: updatedFiles})
-    e.stopPropagation()
+  handleDel (e, index) {
+    e.stopPropagation();
+
+    let updatedFiles = this.state.files;
+    updatedFiles.splice(index, 1);
+    this.setState({files: updatedFiles});
   }
   handleUpload () {
-    this.setState({loading: true })
+    this.setState({loading: true });
     this.state.files.forEach(file => {
-      let {name, preview} = file
+      let {name, preview} = file;
       let config = {
         responseType: 'blob'
-      }
+      };
       axios.get(preview, config)
         .then(res => res.data)
         .then(
           file => {
-            let data = new FormData()
-            data.append('file', file)
-            data.append('filename', name)
-            data.append('semester', this.state.semester)
-            data.append('type', this.state.type)
-            data.append('course', this.state.course)
-            data.append('instructor', this.state.instructor)
-            data.append('type', this.state.category)
-            data.append('uid', this.props.uid)
+            let data = new FormData();
+            data.append('file', file);
+            data.append('filename', name);
+            data.append('semester', this.state.semester);
+            data.append('type', this.state.type);
+            data.append('course', this.state.course);
+            data.append('instructor', this.state.instructor);
+            data.append('type', this.state.category);
+            data.append('uid', this.props.uid);
 
             const options = {
               method: 'POST',
               headers: { 'content-type': 'multipart/form-data' },
               data: data,
               url: `${baseURL}/upload`
-            }
+            };
             axios(options).then(res => {
-              this.setState({loading: false })
+              this.setState({loading: false });
               if (res.statusText === 'OK') {
-                toast('上傳成功')
-                closeModal()
+                toast('上傳成功');
+                closeModal();
               } else {
-                toast('上傳失敗', {type: 'Danger'})
+                toast('上傳失敗', {type: 'Danger'});
               }
             }).catch(() => {
-              this.setState({loading: false })
-              toast('上傳失敗', {type: 'Danger'})
-            })
+              this.setState({loading: false });
+              toast('上傳失敗', {type: 'Danger'});
+            });
           }
         )
-        .catch(err => console.log(err))})
+        .catch(err => console.log(err));});
   }
   render () {
-    const {classes, courses, teachers} = this.props
-    const {files} = this.state
+    const { courses, teachers} = this.props;
     return (
       <div>
-        <div className={classes.header}>
-          <div className={classNames('fas fa-times', classes.leftAction)} onClick={closeModal} />
-          <span className={classes.title}>
-            上傳考古題
-          </span>
-          <span className={classNames(classes.action, !this.state.loading && classes.activeAction)} {...!this.state.loading && { onClick: this.handleUpload }}>
+        <Header>
+          <Leave onClick={closeModal}/>
+          <Title>上傳考古題</Title>
+          <Action active={!this.state.loading} {...!this.state.loading && { onClick: this.handleUpload }}>
             {this.state.loading ? <Loading/> : <><i className='fas fa-cloud-upload-alt' />&nbsp;上傳</>}
-          </span>
-        </div>
+          </Action>
+        </Header>
         <form action=''>
-          <div className={classes.inputContainer}>
-            <Input label='西元年份' autoComplete={false} onChange={(v) => {this.setState({semester: v})}}/>
-            <Input label='類型' autoComplete={['期中考','期末考','小考']} onChange={(v) => {this.setState({type: v})}}/>
-            <Input label='課名' autoComplete={courses.map(c => c.zh)} onChange={(v) => {this.setState({course: v})}}/>
-            <Input label='分類' autoComplete={COURSE_TYPES} onChange={(v) => {this.setState({category: v})}}/>
-            <Input label='老師' autoComplete={teachers} onChange={(v) => {this.setState({instructor: v})}}/>
-          </div>
-          <Dropzone
-            accept='image/*,application/*'
-            onDrop={this.handleDrop}
-            className={classNames(classes.upload, files.length > 1 && 'active')}
-            activeClassName={classes.activeUpload}
-            rejectClassName={classes.rejectUpload}
-            multiple={false}
-            // disabledClassName={classes.disabledUpload}
-            maxSize={10 * 1000 * 1000} // maxsize is 10MB
-          >
-            {files.length > 0
-              ? files.map(({id, name, size, type, preview}) => (
-                <div className={classes.file} key={id}>
-                  <div className={classes.del} onClick={(e) => this.handleDel(e, id)}>
-                    <i className='fas fa-times' />
-                  </div>
-                  <div className={classes.meta}>
-                    <div className={classes.label}>{name}</div>
-                    <div className={classes.label}>{size}MB</div>
-                  </div>
-                  <a className={classes.type} href={preview} rel='noopener noreferrer' target='_blank' onClick={(e) => { e.stopPropagation() }}>
-                    {
-                      type[0] === 'image' ? <img src={preview} style={{width: '3rem'}} alt='preview' />
-                        : type[1] === 'pdf' ? <i className='fas fa-file-pdf' /> : <i className='fas fa-file-archive' />}
-                  </a>
-                </div>
-              ))
-              : (
-                <div>
-                  <p>拖曳檔案至方框內或點擊方框選取檔案上傳</p>
-                  <p>Try dropping some files here, or click to select files to upload.</p>
-                </div>
-              )
-            }
-
-          </Dropzone>
+          <InputContainer>
+            <Input label='西元年份' autoComplete={false} onChange={(v) => {this.setState({semester: v});}}/>
+            <Input label='類型' autoComplete={['期中考','期末考','小考']} onChange={(v) => {this.setState({type: v});}}/>
+            <Input label='課名' autoComplete={courses.map(c => c.zh)} onChange={(v) => {this.setState({course: v});}}/>
+            <Input label='分類' autoComplete={COURSE_TYPES} onChange={(v) => {this.setState({category: v});}}/>
+            <Input label='老師' autoComplete={teachers} onChange={(v) => {this.setState({instructor: v});}}/>
+          </InputContainer>
+          <Uploader onDrop={this.handleDrop} onDel={this.handleDel}/>
         </form>
       </div>
-    )
+    );
   }
 }
 
@@ -179,5 +119,5 @@ export default connect((state) => {
     uid: state.auth.id,
     courses: state.main.allCourse,
     teachers: state.main.teachers.map(t => t.name),
-  }
-})(injectSheet(style)(uploadModal))
+  };
+})(uploadModal);
--- src/Component/Upload/style.js
+++ src/Component/Upload/style.js
@@ -1,153 +1,169 @@
-const style = theme => ({
-  header: {
-    height: 'initial',
-    textAlign: 'left',
-    padding: '0 10px',
-    fontWeight: 500,
-    background: theme.colorPrimary,
-    // borderRadius: '5px 5px 0 0',
-    font: {
-      size: 18,
-      weight: 400
-    },
-    color: 'white',
-    margin: '0 0 20px 0',
-    boxShadow: theme.shadow
-  },
-  title: {
-    height: 'initial',
-    textAlign: 'left',
-    padding: '0 20px',
-    fontWeight: 500,
-    // borderRadius: '5px 5px 0 0',
-    color: 'white',
-    fontSize: 20,
-  },
-  leftAction: {
-    display: 'inline-block',
-    height: 20,
-    width: 20,
-    textAlign: 'center',
-    lineHeight: '20px',
-    padding: 5,
-    borderRadius: 20,
-    '&:hover': {
-      transition: '.2s',
-      cursor: 'pointer',
-      backgroundColor: theme.colorDarkenPrimary
-    }
-  },
-  action: {
-    position: 'absolute',
-    height: 65,
-    padding: '0 2%',
-    top: 0,
-    right: '5%',
-  },
-  activeAction: {
-    '&:hover': {
-      transition: '.2s',
-      cursor: 'pointer',
-      backgroundColor: theme.colorDarkenPrimary
-    }
-  },
-  inputContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '0 auto',
-    width: '90%'
-  },
-  upload: {
-    border: '2px dashed #ccc',
-    borderRadius: 5,
-    margin: '10px auto 30px auto',
-    width: '90%',
-    transition: '.2s',
-    overflowY: 'auto',
-    maxHeight: '50vh',
-    cursor: 'pointer',
-    '&.active': {
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr'
-    }
-  },
-  activeUpload: {
-    border: `2px solid ${theme.colorPrimary}`,
-    backgroundColor: '#eee'
-  },
-  rejectUpload: {
-    border: '2px solid #D50000',
-    backgroundColor: '#eee'
-  },
-  '@keyframes fadeInLeft': {
-    from: {
-      opacity: '0',
-      transform: 'translateX(-30px)'
-    },
-    to: {
-      opacity: '1',
-      transform: 'translateY(0)'
-    }
-  },
-  file: {
-    position: 'relative',
-    backgroundColor: 'white',
-    borderRadius: '5px',
-    border: '2px solid #eee',
-    padding: '10px',
-    width: '18rem',
-    height: '3rem',
-    margin: '10px auto 10px auto',
-    animation: 'fadeInLeft .5s ease-in-out'
-  },
-  del: {
-    position: 'absolute',
-    top: '1.3rem',
-    backgroundColor: '#ccc',
-    transition: '.5s',
-    color: 'white',
-    fontSize: '0.8rem',
-    lineHeight: '1.5rem',
-    height: '1.5rem',
-    width: '1.5rem',
-    textAlign: 'center',
-    borderRadius: '50%',
-    '&:hover': {
-      backgroundColor: '#5bc8a5',
-      transition: '.5s',
-      transform: 'scale(1.1)'
-    }
-  },
-  meta: {
-    position: 'absolute',
-    left: '5rem'
-  },
-  label: {
-    fontSize: '1rem',
-    lineHeight: '1.5em',
-    textAlign: 'left',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-    width: '8rem',
-    whiteSpace: 'nowrap',
-    '&:last-child': {
-      opacity: '0.4'
-    }
-  },
-  type: {
-    display: 'block',
-    position: 'absolute',
-    right: '1rem',
-    color: '#aaa',
-    fontSize: '2rem',
-    lineHeight: '3rem',
-    transition: '.2s',
-    '& :hover': {
-      transition: '.2s',
-      transform: 'scale(1.1)',
-      color: '#5bc8a5'
-    }
-  }
-})
+import styled, { keyframes } from 'styled-components';
 
-export default style
+const fadeInLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const Header = styled.div`
+  color: white;
+  height: initial;
+  margin: 0 0 20px 0;
+  padding: 0 10px;
+  font-size: 18px;
+  text-align: left;
+  font-weight: 500;
+  background: ${props => props.theme.colorPrimary};
+  box-shadow: ${props => props.theme.shadow};
+`;
+export const Title = styled.span`
+  color: white;
+  height: initial;
+  padding: 0 20px;
+  font-size: 20px;
+  text-align: left;
+  font-weight: 500;
+`;
+
+export const Leave = styled.div.attrs({
+  className: 'fas fa-times',
+})`
+  display: inline-block;
+  height: 24px;
+  width: 24px;
+  text-align: center;
+  line-height: 24px;
+  border-radius: 24px;
+
+  &:hover {
+    cursor: pointer;
+    transition: .2s;
+    background: ${props => props.theme.colorDarkenPrimary};
+  }
+`;
+
+export const Action = styled.span`
+  top: 0;
+  right: 5%;
+  height: 65px;
+  padding: 0 2%;
+  position: absolute;
+
+  ${props => props.active && `
+    &:hover {
+      cursor: pointer;
+      transition: .2s;
+      background: ${props.theme.colorDarkenPrimary};
+    }
+  `}
+`;
+
+export const InputContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const File = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 18rem;
+  height: 70px;
+  border: 2px solid #eee;
+  margin: 10px 0 10px 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: white;
+  animation: ${fadeInLeft} .5s ease-in-out;
+`;
+
+export const Del = styled.div`
+  color: white;
+  width: 1.5rem;
+  height: 1.5rem;
+  font-size: 0.8rem;
+  text-align: center;
+  transition: .5s;
+  line-height: 1.5rem;
+  border-radius: 50%;
+  background-color: #ccc;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: .5s;
+    background-color: ${props => props.theme.colorSecondary};
+  }
+`;
+
+export const Meta = styled.div`
+  left: 5rem;
+  position: absolute;
+`;
+
+export const Label = styled.div`
+  width: 8rem;
+  overflow: hidden;
+  font-size: 1rem;
+  text-align: left;
+  line-height: 1.5em;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  &:last-child {
+    opacity: 0.4;
+  }
+`;
+
+export const Type = styled.a`
+  color: #aaa;
+  right: 1rem;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  font-size: 2rem;
+  transition: .2s;
+  line-height: 65px;
+  max-height: 65px;
+
+  &:hover {
+    color: #5bc8a5;
+    transform: scale(1.1);
+    transition: .2s;
+  }
+
+  img {
+    height: 60px;
+    max-width: 60px;
+  }
+`;
+
+export const Container = styled.label`
+  cursor: pointer;
+  display: flex;
+  flex-wrap: wrap;
+  outline: none;
+  min-height: 180px;
+  max-height: 50vh;
+  width: 90%;
+  margin: 10px auto 30px;
+  border: 2px dashed #ccc;
+  border-radius: 5px;
+  transition: .2s;
+`;
+
+export const Description = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+export const InvisibleInput = styled.input`
+  display: none;
+`;
