ad97936d0270885419d8c15fb9e63e296a3d297a
maxam2017 <whk0313@gmail.com>
2019-11-23 15:21:03 +0800

    chore(upload): to function component

--- src/Component/Toast/index.js
+++ src/Component/Toast/index.js
@@ -8,7 +8,7 @@
     autoHide[number] the duration(unit:sec) until auto removing, default value is 5sec
     static  [boolean] if set true, disable auto hidden
 */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Wrapper, Item } from './style';
 
 const palette = {
@@ -30,83 +30,66 @@ const getter = {
   }
 };
 
-class Toast extends React.Component {
-  constructor (props) {
-    super(props);
-    this.handleClose = this.handleClose.bind(this);
-  }
-  componentDidMount () {
-    if (!this.props.static) {
-      let autoHideDuration = this.props.autoHide * 1000;
-      setTimeout(this.handleClose, autoHideDuration);
+const Toast = ({ id, type, isStatic, autoHide, close, children }) => {
+  const handleClose = () => close(id);
+  useEffect(() => {
+    if (!isStatic) {
+      let autoHideDuration = autoHide * 1000;
+      setTimeout(handleClose, autoHideDuration);
     }
-  }
-  handleClose () {
-    this.props.close(this.props.id);
-  }
-  render () {
-    return (
-      <Item
-        colors={palette[this.props.type]}
-        onClick={this.handleClose}>
-        {this.props.children}
-      </Item>
-    );
-  }
-}
+  }, []);
 
-class ToastWrapper extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      seq: 0,
-      notifyList: []
-    };
-    this.addToast = this.addToast.bind(this);
-    this.removeToast = this.removeToast.bind(this);
-  }
-  componentDidMount () {
+  return (
+    <Item colors={palette[type]} onClick={handleClose}>
+      {children}
+    </Item>
+  );
+};
+
+const ToastWrapper = () => {
+  const [ notifyList, setNotifyList ] = useState({ data: [], seq: 0 });
+
+  const addToast = notification => {
+    setNotifyList(({data, seq}) => ({
+      data: [...data, { ...notification, id: seq }],
+      seq: seq + 1,
+    }));
+  };
+
+  const removeToast = id => {
+    setNotifyList(notifyList => ({
+      ...notifyList,
+      data: notifyList.data.filter(notification => notification.id !== id),
+    }));
+  };
+
+  useEffect(() => {
     getter
-      .add(notification => this.addToast(notification))
-      .add(id => this.removeToast(id));
-  }
-  componentWillUnmount () {
-    this.setState = () => {};
-    getter.remove();
-  }
-  addToast (notification) {
-    let updatedList = this.state.notifyList;
-    updatedList.push({ ...notification, id: this.state.seq });
-    this.setState({ seq: this.state.seq + 1, notifyList: [...updatedList] });
-  }
-  removeToast (id) {
-    let updatedList = this.state.notifyList.filter(notification => notification.id !== id);
-    this.setState({ notifyList: [...updatedList] });
-  }
-  render () {
-    return (
-      <Wrapper>
-        {
-          this.state.notifyList.length > 0 &&
-        this.state.notifyList.sort((a, b) => a.id < b.id)
-          .map(
-            notification =>
-              <Toast
-                key={notification.id}
-                id={notification.id}
-                close={this.removeToast}
-                type={notification.type || 'Success'}
-                autoHide={notification.autoHide || 5}
-                static={notification.static}>
-                {notification.content}
-              </Toast>
-          )
-        }
-      </Wrapper>
-    );
-  }
-}
+      .add(notification => addToast(notification))
+      .add(id => removeToast(id));
+    return () => {
+      // setNotifyList = () => {};
+      getter.remove();
+    };
+  }, []);
 
+  return (
+    <Wrapper>
+      {notifyList.data.length > 0 && notifyList.data.sort((a, b) => a.id < b.id)
+        .map(notification =>
+          <Toast
+            key={notification.id}
+            id={notification.id}
+            close={removeToast}
+            type={notification.type || 'Success'}
+            autoHide={notification.autoHide || 5}
+            static={notification.static}>
+            {notification.content}
+          </Toast>
+        )}
+    </Wrapper>
+  );
+};
 
 const toast = (content, options) => {
   getter.func[0]({ content: content, ...options });
